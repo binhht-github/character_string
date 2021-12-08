25b344bb293276a235e1eb2319a6d24bf23890f3
binhhtph11879 <67915869+binhhtph1179@users.noreply.github.com>
2021-12-08 08:59:11 +0700

    pushcode

---
+++ src/main/java/com/example/demo/VnCharacterUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnCharacterUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnCharacterUtilsApplication.class, args);
+	}
+
+}
